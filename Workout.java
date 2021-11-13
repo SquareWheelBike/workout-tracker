03e5fd8b42a997588ab2261125f323b0da0d7f2b
Cole Fuerth <colefuerth@gmail.com>
2021-11-12 22:46:25 -0500

    started working on input repository of exercises

--- Workout.java
+++ Workout.java
@@ -6,7 +6,11 @@ public class Workout {
     public static void main(String[] args){
         new initUI();
         while(true){
-            
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
 }
