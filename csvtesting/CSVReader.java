e59195d4b97ceae48341e4d8efd4766a3121eceb
Cole Fuerth <colefuerth@gmail.com>
2021-11-15 18:14:33 -0500

    first iteration of csv reader

---
+++ csvtesting/CSVReader.java
@@ -0,0 +1,36 @@
+package csvtesting;
+
+import java.util.*;
+import java.io.*;
+
+public class CSVReader {
+    // import a CSV file as an ArrayList of ArrayLists
+    public static ArrayList<ArrayList<String>> readCSV(String fileName) {
+        ArrayList<ArrayList<String>> csv = new ArrayList<ArrayList<String>>();
+        try {
+            // open the file
+            FileInputStream fis = new FileInputStream(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            // read each line
+            while ((line = br.readLine()) != null) {
+                // split the line into an array
+                String[] tokens = line.split(",");
+                // create a new ArrayList for each line
+                ArrayList<String> lineArray = new ArrayList<String>();
+                // add each token to the line ArrayList
+                for (String token : tokens) {
+                    lineArray.add(token);
+                }
+                // add the line ArrayList to the csv ArrayList
+                csv.add(lineArray);
+            }
+            // close the file
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return csv;
+    }
+}
---
+++ csvtesting/Test.java
@@ -0,0 +1,8 @@
+package csvtesting;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(CSVReader.readCSV("List.csv"));
+        System.exit(0);
+    }
+}
